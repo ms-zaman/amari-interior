ff231b3e650c705b2ee99e93b3dc5b1f756a4f1a
ms-zaman <dev.mszaman@gmail.com>
2025-05-15 06:27:36 +0600

    Configure project for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "build",
-  },
+    plugins: [react()],
+    base: "/amari-interior/", // Base path for GitHub Pages
+    build: {
+        outDir: "build",
+    },
 });
