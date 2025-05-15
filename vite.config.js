f190be7b2e7972d65ae39daf8eaaf3dafc488fd0
ms-zaman <dev.mszaman@gmail.com>
2025-05-15 06:56:21 +0600

    Update configuration for Netlify deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: "/amari-interior/", // Base path for GitHub Pages
     build: {
         outDir: "build",
     },
